f1b39b4fa7cb4b7484b3102ca9a61d72f8ba4b1d
Claudio Lopes <devclaudiolopes@gmail.com>
2025-11-29 14:57:15 -0300

    conteúdo até o módulo advanced

---
+++ web/cypress/e2e/iframe.cy.js
@@ -0,0 +1,25 @@
+describe('iframe', () => {
+
+    it('Deve poder tocar o video de exemplo', () => {
+        cy.login()
+        cy.contains('Video').click()
+
+        //think time
+        cy.wait(3000)
+
+        cy.get('iframe[title="Video Player"]')
+            .should('exist')
+            .its('0.contentDocument.body')
+            .then(cy.wrap)
+            .as('iframePlayer')
+
+        cy.get('@iframePlayer')
+            .find('.play-button')
+            .click()
+            
+         cy.get('@iframePlayer')
+            .find('.pause-button')
+            .should('be.visible')   
+    })
+
+})
